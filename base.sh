4f63e289cbba33e5e0d2041887d95ef6ade33ab2
Ruben Rosales <ruben.ros14@gmail.com>
2024-06-17 11:54:56 -0700

    Create base.sh

---
+++ base.sh
@@ -0,0 +1,7 @@
+apt-get update 
+apt update
+apt-get install software-properties-common -y
+apt-get install tmux nano vim curl wget -y
+# apt install build-essential cmake git libgtk-3-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev openexr libatlas-base-dev libopenexr-dev libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev python3-dev python3-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-dev gfortran -y
+
+apt install libopencv-dev
