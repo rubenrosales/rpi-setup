14525f68125fc510421e9649d4af6cbb2d497d07
ruben <ruben.rosales@ieee.org>
2024-07-08 21:19:01 +0100

    adding example to benchmark

---
+++ benchmark/run.sh
@@ -0,0 +1,3 @@
+# venv: python -m pip install scalene
+# docker: python3 -m pip install scalene
+scalene --profile-all --html --outfile profile.html python_file.py --- -y path_to_config.yaml
---
+++ docker/setup.sh
@@ -0,0 +1,3 @@
+instructions: https://docs.docker.com/engine/install/debian/
+
+https://github.com/isl-org/Open3D/tree/main/dockerdock
